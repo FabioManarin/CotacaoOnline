363a8fc8858a089b5c09cf692226ccc93ac0c06e
fabio <fabiosantosmanarin@gmail.com>
2019-02-15 21:06:01 -0200

    Implementado cadastro de produto.

--- CotacaoOnline/WebContent/js/controller/MainController.js
+++ CotacaoOnline/WebContent/js/controller/MainController.js
@@ -4,9 +4,58 @@ angular.module('MainController', []).controller('MainController', function ($sco
 	
     $scope.listar = () => {
     	let fn = null;
-        service.getProduto(function(resp){
+        service.listarProduto(function(resp){
         	$scope.listaProdutos = resp.data;
         });
+    }
+    
+    $scope.salvar = function(){
+    	if ($scope.produto.id > 0) {
+    		service.alterarProduto($scope.produto,function(resp){
+				limpar();
+		    	$scope.listar();
+		    	fecharCadastro();
+    		});
+    	} else {
+    		service.inserirProduto($scope.produto,function(resp){
+				limpar();
+		    	$scope.listar();
+		    	fecharCadastro();
+    		});
+    	}
+    }
+    
+    $scope.removerProduto = function(id) {
+    	service.removerProduto(id, function(resp) {
+    		$scope.listar();
+    	});
+    }
+    
+    $scope.onClickEditar = function(id) {
+    	service.listarProdutoId(id, function(resp) {
+    		$scope.produto = resp.data
+    	});
+    	abrirCadastro();
+    }
+    
+    $scope.onClickFechar = function(id) {
+    	fecharCadastro();
+    }
+    
+    function fecharCadastro () {
+    	let modal = document.getElementsByClassName("modal")[0];
+
+    	modal.style.display = "none";
+    }
+    
+    function abrirCadastro () {
+    	let modal = document.getElementsByClassName("modal")[0];
+
+    	modal.style.display = "block";
+    }  
+    
+    function limpar() {
+    	$scope.produto = {};
     }
     
     $scope.listar();
--- CotacaoOnline/WebContent/js/services/MainService.js
+++ CotacaoOnline/WebContent/js/services/MainService.js
@@ -1,25 +1,26 @@
 angular.module('MainService', []).factory('service', ['$http', function($http){
-	const api = "/CotacaoOnline/rest/produto/";
+    const api = "/CotacaoOnline/rest/produto/";
+    //const api = " http://contacaoonlineteste.jelasticlw.com.br/rest/produto/";
 		
     return {   
-        getProduto: function(cbSuccess, cbError) {
-            return $http.get(api + 'listarProduto', )
+        listarProduto: function(cbSuccess, cbError) {
+            return $http.get(api + 'listarProduto')
                     .then(cbSuccess)
         },
-        getComprasId: function(id, cbSuccess, cbError) {
+        listarProdutoId: function(id, cbSuccess, cbError) {
             return $http.get(api + id)
                     .then(cbSuccess)
         },
-        addCompras: function(produto, cbSuccess, cbError) {
-            return $http.post(api + 'add/', produto)
+        inserirProduto: function(produto, cbSuccess, cbError) {
+            return $http.post(api + 'inserirProduto/', produto)
                     .then(cbSuccess)
         },
-        excluirProduto: function(id, cbSuccess, cbError) {
-            return $http.delete(api + 'delete/' + id)
+        removerProduto: function(id, cbSuccess, cbError) {
+            return $http.delete(api + 'removerProduto/' + id)
                     .then(cbSuccess)
         },
-        editarCompras: function(cbSuccess, cbError) {
-            return $http.post(api + 'edit/')
+        alterarProduto: function(produto, cbSuccess, cbError) {
+            return $http.post(api + 'alterarProduto/', produto)
                     .then(cbSuccess)
         }
     }
